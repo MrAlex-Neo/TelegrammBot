38d5131b96891ea8fee5c8057bc13fe1df7d79e2
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-04-26 01:39:36 +0500

    filePushUser

--- js/script.js
+++ js/script.js
@@ -98,3 +98,17 @@ Telegram.WebApp.onEvent ('mainButtonClicked', function() {
         state = states[2]
     }
 })
+
+
+
+
+
+
+$(document).on('click', '#btn', function(){
+    var formData = new FormData();
+    formData.append("myFile", document.getElementById("file").files[0], 'chris1.jpg');
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "index.php");
+    xhr.send(formData);
+});
